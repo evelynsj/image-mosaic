83c966245b0237d95599df8e940e139d5b1aced0
Evelyn Sjafii <evelynsj98@gmail.com>
2019-05-19 17:58:23 -0700

    compute bounding box

--- warpImage.m
+++ warpImage.m
@@ -13,12 +13,26 @@ for i=1:size(pts,2)
     fwdWarp(:,i) = res(1:2,:);
 end
 
-% figure;
 % imshow(refIm)
 % hold on;
 % plot(fwdWarp(1,:), fwdWarp(2,:),'r.', 'MarkerSize', 25);
 
 %% determine bounding box
+[r,c,~] = size(refIm);
+pts = [1 1; c 1; 1 r; c r]'; % four corners of ref image
+x = [fwdWarp(1,:) pts(1,:)];
+y = [fwdWarp(2,:) pts(2,:)];
+
+% order: top-left, top-right, bottom-left, bottom-right
+box_x = [min(x) max(x) min(x) max(x)];
+box_y = [min(y) min(y) max(y) max(y)];
+
+box = [box_x; box_y];
+
+% imshow(refIm)
+% hold on;
+% plot(pts(1,:), pts(2,:),'r.', 'MarkerSize', 25);
+% plot(box_x, box_y,'b.', 'MarkerSize', 25);
 
 %% do inverse warp
 
